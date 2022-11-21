199630a4af46e794380a38f2e2d227e34a24f58b
cmurphy88 <104079607+cmurphy88@users.noreply.github.com>
2022-11-21 19:48:11 +0000

    updates to CRUD processes for exercise endpoints

--- api/app/exercise/schema.py
+++ api/app/exercise/schema.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -11,3 +13,11 @@ class DisplayExercise(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class UpdateExercise(BaseModel):
+    id: Optional[int]
+    name: Optional[str]
+
+    class Config:
+        orm_mode: True
--- api/app/exercise/services.py
+++ api/app/exercise/services.py
@@ -3,6 +3,14 @@ from fastapi import HTTPException, status
 from . import models
 
 
+async def create_new_exercise(request, database) -> models.Exercise:
+    new_exercise = models.Exercise(name=request.name)
+    database.add(new_exercise)
+    database.commit()
+    database.refresh(new_exercise)
+    return new_exercise
+
+
 async def get_all_exercises(database) -> List[models.Exercise]:
     exercises = database.query(models.Exercise).all()
     return exercises
@@ -13,9 +21,14 @@ async def get_exercise(exercise_id, database) -> models.Exercise:
     return exercise
 
 
-def update_exercise(request, database):
-    return None
+# async def update_exercise(request, database) -> models.Exercise:
+#     old_exercise = database.query(models.Exercise).get(request.id)
+#     if not old_exercise:
+#         raise status.HTTP_404_NOT_FOUND
+#     request_data = request.dict(exclude_unset=True)
+# try and do a delete, and create a new exercise???
 
 
-def delete_exercise(exercise_id, database):
-    database.query(models.Exercise).filter(models.Exercise.id == exercise_id).delete()
+async def delete_exercise(exercise_id, database):
+    database.query(models.Exercise).filter(models.Exercise.id == exercise_id)
+    database.commit()
