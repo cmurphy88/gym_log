2a343ce41c7c3f7956e186b4f8675b9d0f4a3c24
cmurphy88 <104079607+cmurphy88@users.noreply.github.com>
2022-12-03 22:45:29 +0000

    delete UI

--- ui/src/config/idex.jsx
+++
@@ -1,5 +0,0 @@
-function getApiUrl() {
-    return process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/";
-}
-
-export {getApiUrl}; 
--- ui/src/shared/constants/index.jsx
+++
@@ -1,2 +0,0 @@
-export const USERNAME = "Username";
-export const ROUTINES = "Routines";
